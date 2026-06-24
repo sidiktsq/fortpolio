0a5ac3e9292b08a5ba1fdf092b05c403f40048a9
sidiktsq <baika3542@gmail.com>
2026-06-24 20:23:59 +0700

    feat: add global styles and Lenis smooth scrolling integration

---
+++ app/plugins/lenis.client.ts
@@ -0,0 +1,23 @@
+import Lenis from 'lenis'
+
+export default defineNuxtPlugin(() => {
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    touchMultiplier: 2,
+    infinite: false,
+  })
+
+  function raf(time: number) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+
+  requestAnimationFrame(raf)
+
+  return {
+    provide: {
+      lenis,
+    },
+  }
+})
